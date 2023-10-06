e3e4c4ebb319f58588e11b7631bc120efd65d8d7
Artiom-Evs <evs.a.s@mail.ru>
2023-10-06 15:29:36 +0200

    Implement routing
    Routing functionality has been implemented using react-router-dom library.

--- umanager.web/src/main.jsx
+++ umanager.web/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const rootElement = document.getElementById('root');
+const root =  ReactDOM.createRoot(rootElement);
+
+root.render(
+  <BrowserRouter basename="/">
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>);
